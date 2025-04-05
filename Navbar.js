e2a5115d5a4fb5d7f1e2f039c668af85165a7b27
sogdiiiana <s.ravshanova@newuu.uz>
2025-04-05 23:14:07 +0300

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -1,30 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import './Navbar.css';
+import React, { useState } from "react";
+import "./Navbar.css";
 
-export default function Navbar() {
+export default function Navbar({ onSectionSelect, activeButton }) {
+  const [isMaleDropdownVisible, setIsMaleDropdownVisible] = useState(false);
+  const [isFemaleDropdownVisible, setIsFemaleDropdownVisible] = useState(false);
+
   return (
     <nav className="navbar">
       <h1>Old-School Barbershop</h1>
       <div className="menu">
-        {/* Male Link */}
-        <div className="dropdown">
-          <Link to="/male">
-            <button>
-              Male
-            </button>
-          </Link>
+        {/* Male Dropdown */}
+        <div
+          className="dropdown"
+          onMouseEnter={() => setIsMaleDropdownVisible(true)}
+          onMouseLeave={() => setIsMaleDropdownVisible(false)}
+        >
+          <button className={activeButton === "male" ? "active" : ""}>
+            Male
+          </button>
+          {isMaleDropdownVisible && (
+            <div className="sub-navbar">
+              <button>Grooming</button>
+              <button>Haircuts</button>
+              <button>Shaving</button>
+              <button>Beard Trim</button>
+              <button>Waxing</button>
+            </div>
+          )}
         </div>
 
-        {/* Female Link */}
-        <div className="dropdown">
-          <Link to="/female">
-            <button>
-              Female
-            </button>
-          </Link>
+        {/* Female Dropdown */}
+        <div
+          className="dropdown"
+          onMouseEnter={() => setIsFemaleDropdownVisible(true)}
+          onMouseLeave={() => setIsFemaleDropdownVisible(false)}
+        >
+          <button className={activeButton === "female" ? "active" : ""}>
+            Female
+          </button>
+          {isFemaleDropdownVisible && (
+            <div className="sub-navbar">
+              <button>Hair Styling</button>
+              <button>Nail Care</button>
+              <button>Makeup</button>
+              <button>Facial Treatments</button>
+              <button>Massages</button>
+            </div>
+          )}
         </div>
       </div>
     </nav>
   );
 }
+
