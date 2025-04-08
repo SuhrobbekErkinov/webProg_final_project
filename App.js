7e4af8c878ca340e8d346e17f0544a443c40be7e
sogdiiiana <s.ravshanova@newuu.uz>
2025-04-09 00:17:34 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar'; // Import Navbar
-import MalePage from './MalePage'; // Import MalePage
-import FemalePage from './FemalePage'; // Import FemalePage
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar";
+import MalePage from "./MalePage";
+import FemalePage from "./FemalePage";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
         <Route path="/male" element={<MalePage />} />
         <Route path="/female" element={<FemalePage />} />
-        {/* Add any additional routes here */}
       </Routes>
     </Router>
   );
 }
-
-export default App;
-
 
 
 
