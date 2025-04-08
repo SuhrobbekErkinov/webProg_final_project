f7072fc3f8468d8c3fe6031422005911dcd5fb9f
sogdiiiana <s.ravshanova@newuu.uz>
2025-04-09 00:47:57 +0500

    Create BookingPage.js

---
+++ BookingPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BookingPage = () => {
+  return (
+    <div>
+      <h2>Booking Page</h2>
+      <p>Complete your booking here.</p>
+      {/* Booking Form */}
+    </div>
+  );
+};
+
+export default BookingPage;
