7c0701ed9bdda286f14382afaa50d7498df5c52a
sogdiiiana <s.ravshanova@newuu.uz>
2025-04-09 00:47:29 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
+// src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./Navbar";
-import MalePage from "./MalePage";
-import FemalePage from "./FemalePage";
+import MalePage from "./MalePage"; // Import MalePage
+import FemalePage from "./FemalePage"; // Import FemalePage
+import BookingPage from "./BookingPage"; // Import BookingPage
+import Navbar from "./Navbar"; // Import Navbar
 
-export default function App() {
+function App() {
   return (
     <Router>
-      <Navbar />
+      <Navbar /> {/* Navbar is placed above Routes */}
       <Routes>
-        <Route path="/male" element={<MalePage />} />
-        <Route path="/female" element={<FemalePage />} />
+        <Route path="/male" element={<MalePage />} /> {/* Route for MalePage */}
+        <Route path="/female" element={<FemalePage />} /> {/* Route for FemalePage */}
+        <Route path="/booking" element={<BookingPage />} /> {/* Route for BookingPage */}
       </Routes>
     </Router>
   );
 }
 
-
-
+export default App;
 
