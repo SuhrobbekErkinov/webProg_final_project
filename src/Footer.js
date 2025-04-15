486eaa75a3a4e76299a5cd3c51e0f9126da615fe
sogdiiiana <s.ravshanova@newuu.uz>
2025-04-15 08:46:34 +0300

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -8,17 +8,16 @@ export default function Footer() {
       <div className="footer-content">
         <div>
           <h3>Contact Us</h3>
-          <p>(234) 658-7199</p>
-          <p>info@minerva.com</p>
-          <p>care@minerva.com</p>
-          <p>200 W 85th ST NEW YORK NY 10024-3139 USA</p>
+          <p>+998 (71) 202-41-11</p>
+          <p>info@newuu.uz</p>
+          <p>Tashkent city, Mirzo Ulugbek district, Movarounnahr street 1</p>
         </div>
         <div>
           <h3>Subscribe to the newsletter</h3>
           <input type="email" placeholder="Email..." />
         </div>
       </div>
-      <p className="footer-note">© MINERVA. All Rights Reserved.</p>
+      <p className="footer-note">© SM SALON. All Rights Reserved.</p>
     </footer>
   );
 }
