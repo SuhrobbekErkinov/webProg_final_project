56cef95ffdc30776fcc5673d7464784510af3ee9
Saidmurod <s.xamidov@newuu.uz>
2025-04-21 08:58:57 +0500

    adding database integration

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -1,13 +1,18 @@
 package org.example.backend;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+//        SpringApplication.run(BackendApplication.class, args);
+        var user = new User();
+        user.setFirstname("saidmurod");
+        user.setLastname("xamidov");
+        user.setEmail("sadifasdf");
+        user.setPassword("234234");
+        System.out.println(user.toString());
     }
 
 }
---
+++ backend/src/main/java/org/example/backend/entities/User.java
@@ -0,0 +1,29 @@
+package org.example.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "firstname", nullable = false)
+    private String firstname;
+
+    @Column(name = "lastname", nullable = false)
+    private String lastname;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+}
